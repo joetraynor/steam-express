f5fed1f90f2ce5c28a34a442fcfdcd46a787cf78
joetraynor <joseph.traynor@o2.com>
2014-05-27 15:12:27 +0100

    looped over all games, got images, can enter steam name and id

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,24 +5,37 @@ var request = require('request');
 var steam_format = '&format=json';
 
 function getGames(steam_id, res){
-  request('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + steam_api_key + '&steamid=' + steam_id + '&include_appinfo=1' +  steam_format, function (error, response, body) {
+  request('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + steam_api_key + '&steamid=' + steam_id + '&include_played_free_games=1' + '&include_appinfo=1' +  steam_format, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var object = JSON.parse(body);
       var response = object.response;
       var RGames = response.games;
-      var gamename = RGames[0].name;
       // console.log(RGames[0].appid);
       res.render('getgames', { title: 'Steam API',
-                                games: body,
+                                games: RGames,
                                 gamecount: response.game_count,
-                                gamename: gamename
                                 });
     }
   })
 };
 
-router.post('/getgames', function(req, res){
-  var games = getGames(req.body.steamID, res);
+function convertSteamID(steam_id, res) {
+ request('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001?vanityurl=' + steam_id + '&key=' + steam_api_key, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+     var object = JSON.parse(body);
+     var response = object.response;
+     var steamID = response.steamid;
+     getGames(steamID, res);
+   }
+ })
+};
+
+router.post('/getGames', function(req, res) {
+  if (isNaN(parseInt(req.body.steamID))) {
+    convertSteamID(req.body.steamID, res)
+  } else {
+    getGames(req.body.steamID, res);
+  };
 });
 
 /* GET home page. */
